94c1007cfb972d8b9922f09a7b0609d424accaf2
drevenb <drevenb@gmail.com>
2021-06-23 11:32:53 +0300

    Initial commit

---
+++ src/main/java/com/codex/test_task/CodexTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.codex.test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codex/test_task/CodexTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codex.test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
